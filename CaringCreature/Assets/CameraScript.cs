0e7474fe6e76b5a135027f7ab4ac7f3f31f9a119
Martin Zielinski <martin@stupidmindlessfun.de>
2015-08-22 12:13:18 +0200

    camera+mommy objects

---
+++ CaringCreature/Assets/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public float DampingFactor = 0.15f;
+
+	private GameObject PlayerObj;
+	private float CurrentVelocity;
+
+	// Use this for initialization
+	void Start () 
+	{
+		PlayerObj = GameObject.FindGameObjectWithTag("Player");
+
+		// set initial camara 
+		transform.position = new Vector3(PlayerObj.transform.position.x, PlayerObj.transform.position.y, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float interpX = Mathf.SmoothDamp(transform.position.x, PlayerObj.transform.position.x, ref CurrentVelocity, DampingFactor);
+		transform.position = new Vector3(interpX, transform.position.y, transform.position.z);
+	}
+}
---
+++ CaringCreature/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	private Rigidbody2D RBComp;
+
+	public float ClickForceStrength = 0.2f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		RBComp = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 cursorPosVec3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		Vector2 playerPos = new Vector2(transform.position.x, transform.position.y);
+		Vector2 cursorPos = new Vector2(cursorPosVec3.x, cursorPosVec3.y);
+
+		//Vector2 forceDir = Vector2.
+		Debug.DrawLine(transform.position, cursorPosVec3, Color.red);
+		//RBComp.AddForce()
+
+	}
+}
