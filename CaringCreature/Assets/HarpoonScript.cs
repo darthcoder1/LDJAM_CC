16c6a1785ce76c0ba55565a15376fa757e8ac263
Martin Zielinski <martin@stupidmindlessfun.de>
2015-08-24 01:47:56 +0200

    harpoon throwing and ship anims

---
+++ CaringCreature/Assets/HarpoonScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HarpoonScript : MonoBehaviour 
+{
+	private Rigidbody2D RBComp;
+	// Use this for initialization
+	void Start () 
+	{
+		RBComp = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (RBComp.velocity.magnitude > 0.1)
+		{
+			Vector2 v = RBComp.velocity;
+			float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg + 135;
+			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		}
+	}
+}
--- CaringCreature/Assets/Scripts/ShipController.cs
+++ CaringCreature/Assets/Scripts/ShipController.cs
@@ -5,18 +5,21 @@ public class ShipController : MonoBehaviour {
 
 	private bool bOverWater;
 	private bool bIsSinking;
+	private bool bHasHarpoon;
 
-	private SpriteRenderer SpriteComp;
-	public Sprite SinkingSprite;
+	private bool bIsThrowing;
+	private bool bIsReloading;
 
 	public PlayerController EatenBy;
 	private float TimeSinceEaten;
 	private Vector2 OriginalScale;
 
 	private Rigidbody2D RBComp;
+	private Animator AnimCtrl;
 	public float WaterFrictionFactor = 0.05f;
 	public float OverWaterGravityScale = 0.35f;
 	public float UnderWaterGravityScale = -0.35f;
+	public float HarpoonReloadTime = 3.0f;
 
 	public float UpwardForce = 3.0f; // 9.81 is the opposite of the default gravity, which is 9.81. If we want the boat not to behave like a submarine the upward force has to be higher than the gravity in order to push the boat to the surface
 
@@ -26,13 +29,13 @@ public class ShipController : MonoBehaviour {
 	private float Direction;
 
 
-
-
 	// Use this for initialization
 	void Start () 
 	{
 		RBComp = GetComponent<Rigidbody2D>();
 		RBComp.gravityScale = OverWaterGravityScale;
+
+		AnimCtrl = GetComponent<Animator>();
 
 		EatenBy = null;
 
@@ -40,11 +43,10 @@ public class ShipController : MonoBehaviour {
 		PlayerController pc = playerObj.GetComponent<PlayerController>();
 
 		WaterLineY = pc.WaterLineY;
-		WaterDetectionThreshold = pc.WaterDetectionThreshold;
-
-		SpriteComp = GetComponent<SpriteRenderer>();
 
 		Direction = (float)Random.Range(-2500, 2500) / 100.0f;
+
+		bHasHarpoon = true;
 	}
 	
 	// Update is called once per frame
@@ -71,6 +73,7 @@ public class ShipController : MonoBehaviour {
 		if (dot < 0.5f && !bIsSinking)
 		{
 			bIsSinking = true;
+			AnimCtrl.SetTrigger("Sinking");
 
 			PlayerController PC = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
 			++PC.ShipsDestroyed;
@@ -108,15 +111,97 @@ public class ShipController : MonoBehaviour {
 			}
 		}
 
+		UpdateHarpoon();
 		UpdateGfx();
+	}
+
+	void UpdateHarpoon()
+	{
+		if (bIsSinking) { return; }
+
+		if (bHasHarpoon && !bIsThrowing)
+		{
+			bIsThrowing = true;
+			Invoke ("ThrowHarpoon", 2.0f);
+		}
+		else if (!bIsReloading)
+		{
+			bIsReloading = true;
+			Invoke ("ReloadHarpoon", HarpoonReloadTime);
+		}
+	}
+
+	void ThrowHarpoon()
+	{
+		GameObject Player = GameObject.FindGameObjectWithTag("Player");
+		if (Player)
+		{
+			ThrowHarpoonAt(Player);
+		}
+		bIsThrowing = false;
+	}
+
+	public Vector3 BallisticVel(Transform from, Transform target)
+	{ 
+		Vector3 dir = target.position - from.position; 
+		float h = dir.y; dir.y =0; 
+		float dist = dir.magnitude; dir.y = dist; dist += h; 
+		float vel = Mathf.Sqrt(dist*Physics.gravity.magnitude); 
+		return vel*dir.normalized; 
+	}
+
+	private Vector3 calculateBestThrowSpeed(Vector3 origin, Vector3 target, float timeToTarget) {
+		// calculate vectors
+		Vector3 toTarget = target - origin;
+		Vector3 toTargetXZ = toTarget;
+		toTargetXZ.y = 0;
+		
+		// calculate xz and y
+		float y = toTarget.y;
+		float xz = toTargetXZ.magnitude;
+		
+		// calculate starting speeds for xz and y. Physics forumulase deltaX = v0 * t + 1/2 * a * t * t
+		// where a is "-gravity" but only on the y plane, and a is 0 in xz plane.
+		// so xz = v0xz * t => v0xz = xz / t
+		// and y = v0y * t - 1/2 * gravity * t * t => v0y * t = y + 1/2 * gravity * t * t => v0y = y / t + 1/2 * gravity * t
+		float t = timeToTarget;
+		float v0y = y / t + 0.5f * Physics.gravity.magnitude * t;
+		float v0xz = xz / t;
+		
+		// create result vector for calculated starting speeds
+		Vector3 result = toTargetXZ.normalized;        // get direction of xz but with magnitude 1
+		result *= v0xz;                                // set magnitude of xz to v0xz (starting speed in xz plane)
+		result.y = v0y;                                // set y to v0y (starting speed of y plane)
+		
+		return result;
+	}
+
+	void ThrowHarpoonAt(GameObject obj)
+	{
+		if (bHasHarpoon)
+		{
+			Transform harpoonTransform = transform.GetChild(0);// GetComponentInChildren<Transform>();
+
+			Debug.DrawLine(harpoonTransform.position, obj.transform.position, Color.red);
+
+			Object prefab = Resources.Load("harpoon");
+			GameObject go = (GameObject)GameObject.Instantiate(prefab, harpoonTransform.position, harpoonTransform.rotation);
+			//go.transform.localScale = harpoonTransform.localScale;
+
+			go.GetComponent<Rigidbody2D>().velocity = calculateBestThrowSpeed(harpoonTransform.position, obj.transform.position, 5);
+			bHasHarpoon = false;
+		}
+	}
+
+	void ReloadHarpoon()
+	{
+		bHasHarpoon = true;
+		bIsReloading = false;
 	}
 
 	void UpdateGfx()
 	{
-		if (bIsSinking)
-		{
-			SpriteComp.sprite = SinkingSprite;
-		}
+		AnimCtrl.SetBool("bHasHarpoon", bHasHarpoon);
 	}
 	
 	void Eaten()
