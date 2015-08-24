6f3f4c3650e6a48632b21c5e0b5f20a2ebb714be
Martin Zielinski <martin@stupidmindlessfun.de>
2015-08-24 18:34:02 +0200

    fixed joergs weird script

--- CaringCreature/Assets/Resources/NestPlaySounds.cs
+++ CaringCreature/Assets/Resources/NestPlaySounds.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class NestPlaySounds : MonoBehaviour {
 
-	public AudioSource[] sound;
-	public int AudioTicks = 0; //counts to add delays at which the audio is played
-	public int TrackIndex = 1; //
+	private AudioSource[] sound;
+	public float SoundPlayInterval = 4.0f;
 
+	private float TimeSinceLastSoundPlayed;
 
 	// Use this for initialization
 	void Start () {
@@ -16,27 +16,13 @@ public class NestPlaySounds : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-		AudioTicks++;
-		//sound[TrackIndex].Play();
-
-		if (AudioTicks == 120) {
-		
-			sound[TrackIndex].Play();
-			AudioTicks = 0;
-
-			if(TrackIndex <5)
-			{
-				TrackIndex++;
-
-			}
-			else
-			{
-				TrackIndex=0;
-			}
-		
-		}	
+	void Update () 
+	{
+		TimeSinceLastSoundPlayed += Time.deltaTime;
+		if (TimeSinceLastSoundPlayed >= SoundPlayInterval)
+		{
+			sound[Random.Range(0,sound.Length)].Play();
+		}
 
 	}
 }
