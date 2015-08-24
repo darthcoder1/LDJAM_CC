4cb9fe13fdefdeac23dac2547181c590ba5c98ea
Martin Zielinski <martin@stupidmindlessfun.de>
2015-08-24 20:03:49 +0200

    audio for nest

--- CaringCreature/Assets/Scripts/NestScript.cs
+++ CaringCreature/Assets/Scripts/NestScript.cs
@@ -7,9 +7,18 @@ public class NestScript : MonoBehaviour
 	public Text HungerDisplay;
 	public float HungerPerSecond = 1.0f / 60;
 
+	public AudioClip[] MunchingSounds;
+	public AudioClip[] DyingSounds;
+	public AudioClip[] HungerSounds;
+	public AudioClip[] DesperateHungerSounds;
+
+	public float HungerSoundPlayInterval = 4.0f;
+	private float HungerSoundLastPlayedTime;
+
 	private float Hunger;
 	private PlayerController PC;
 	private Animator AnimCtrl;
+	private AudioSource AudioComp;
 	private bool bIsEating;
 
 	// Use this for initialization
@@ -19,6 +28,7 @@ public class NestScript : MonoBehaviour
 		PC = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
 
 		AnimCtrl = GetComponent<Animator>();
+		AudioComp = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -41,12 +51,44 @@ public class NestScript : MonoBehaviour
 			GameObject mainCam = GameObject.FindGameObjectWithTag("MainCamera");
 			mainCam.GetComponent<CameraScript>().target = gameObject;
 		}
+
+		UpdateSFX();
+	}
+
+	void UpdateSFX()
+	{
+		HungerSoundLastPlayedTime += Time.deltaTime;
+		if (Hunger <= 0.01f && !bIsEating)
+		{
+			AudioComp.clip = DyingSounds[Random.Range (0, DyingSounds.Length)];
+			AudioComp.Play();
+		}
+		else if (HungerSoundLastPlayedTime > HungerSoundPlayInterval && !bIsEating)
+		{
+			HungerSoundLastPlayedTime = 0.0f;
+
+			AudioClip clipToPlay = null;
+			if (Hunger > 0.5f)
+			{
+				clipToPlay = HungerSounds[Random.Range (0, HungerSounds.Length)];
+			}
+			else
+			{
+				clipToPlay = DesperateHungerSounds[Random.Range(0, DesperateHungerSounds.Length)];
+			}
+			AudioComp.clip = clipToPlay;
+			AudioComp.Play();
+		}
 	}
 
 	void Feed()
 	{
 		Hunger = 1.0f;
 		bIsEating = true;
+
+		AudioComp.clip = MunchingSounds[Random.Range (0, MunchingSounds.Length)];
+		AudioComp.Play();
+		
 	}
 
 	void StopFeeding()
