0406f1b69b8d2fb1020515662ec5e8b42a37f748
freejsf <joerg@stupidmindlessfun.de>
2015-08-24 15:14:03 +0200

    added Martins full of awesome audio script

---
+++ CaringCreature/Assets/VolumeScaler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeScaler : MonoBehaviour 
+{
+	private GameObject Player;
+
+	public float HearingDistance = 30;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float dist = (Player.transform.position - transform.position).magnitude;
+
+		float volume = 1.0f - Mathf.Clamp(dist / HearingDistance,  0, 1);
+		GetComponent<AudioSource> ().volume = volume;
+	}
+}
