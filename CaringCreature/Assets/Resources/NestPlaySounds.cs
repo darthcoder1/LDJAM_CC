bd47803dfb826fd02f5be39eab3d4f51a475dd18
freejsf <joerg@stupidmindlessfun.de>
2015-08-24 17:42:52 +0200

    addded lots of sounds - AND A SCRIPT! BEWARE! WE ALL GONNA DIE!

---
+++ CaringCreature/Assets/Resources/NestPlaySounds.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NestPlaySounds : MonoBehaviour {
+
+	public AudioSource[] sound;
+	public int AudioTicks = 0; //counts to add delays at which the audio is played
+	public int TrackIndex = 1; //
+
+
+	// Use this for initialization
+	void Start () {
+
+		sound=GetComponents<AudioSource>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		AudioTicks++;
+		//sound[TrackIndex].Play();
+
+		if (AudioTicks == 120) {
+		
+			sound[TrackIndex].Play();
+			AudioTicks = 0;
+
+			if(TrackIndex <5)
+			{
+				TrackIndex++;
+
+			}
+			else
+			{
+				TrackIndex=0;
+			}
+		
+		}	
+
+	}
+}
